455db17df2a28fdff7173061dafba41b9fe7321e
Bobby Paris <bobby.paris85@gmail.com>
2023-06-11 17:08:35 +0800

    update package to war

--- src/main/java/com/sf/shopomatic/ShopOMaticApplication.java
+++ src/main/java/com/sf/shopomatic/ShopOMaticApplication.java
@@ -2,9 +2,10 @@ package com.sf.shopomatic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ShopOMaticApplication {
+public class ShopOMaticApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopOMaticApplication.class, args);
