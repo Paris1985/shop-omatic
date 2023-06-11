1e96bec14f1c7edee8fa2ad85504fdfa4e60b53b
Bobby Paris <bobby.paris85@gmail.com>
2023-06-11 14:56:47 +0800

    Fix Quality Gate Issue on Unused block

--- src/main/java/com/sf/shopomatic/controller/ProductController.java
+++ src/main/java/com/sf/shopomatic/controller/ProductController.java
@@ -15,7 +15,4 @@ public class ProductController {
     public List<String> getProducts() {
         return Arrays.asList("Soccer Ball", "Stadium", "Thinking Cap");
     }
-    private List<String> findProducts(int categoryId) {
-        return Arrays.asList("Soccer Ball", "Stadium", "Thinking Cap");
-    }
 }
