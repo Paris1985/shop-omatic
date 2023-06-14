764f408827a4a55d5a5e0f546e67fc2a36d9be38
Bobby Paris <bobby.paris85@gmail.com>
2023-06-14 20:37:08 +0800

    Fix unit test

--- src/test/groovy/com/sf/shopomatic/controller/ProductControllerSpec.groovy
+++ src/test/groovy/com/sf/shopomatic/controller/ProductControllerSpec.groovy
@@ -1,19 +1,25 @@
 package com.sf.shopomatic.controller
 
+import com.sf.shopomatic.service.ProductService
 import org.springframework.ui.Model
 import spock.lang.Specification
 
 class ProductControllerSpec extends Specification{
 
-     ProductController productController = new ProductController()
+     ProductService productService = Mock()
+    ProductRestController productController
 
      def "should return product list"() {
          given:
+             productController = new ProductRestController(
+                     productService: productService
+             )
             Model model = Mock()
          when:
-            def products = productController.getProducts(model)
+            def products = productController.getProducts()
          then:
-            products == "home"
+            1 * productService.getProducts() >> _
+            products == []
 
      }
 }
