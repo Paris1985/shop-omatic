ba77fba7d4c630d3bb98e11ab8a304e60f4eeb6d
Bobby Paris <bobby.paris85@gmail.com>
2023-06-15 20:33:23 +0800

    Fix sonar qube scan issue

--- src/main/java/com/sf/shopomatic/controller/CategoryController.java
+++
@@ -1,4 +0,0 @@
-package com.sf.shopomatic.controller;
-
-public class CategoryController {
-}
--- src/main/java/com/sf/shopomatic/controller/ProductController.java
+++ src/main/java/com/sf/shopomatic/controller/ProductController.java
@@ -14,9 +14,7 @@ public class ProductController {
     private ProductService productService;
     @GetMapping
     public String getProducts(Model model) {
-        String category;
-
-        List<String> products = productService.getProducts();
+         List<String> products = productService.getProducts();
         model.addAttribute("products", products);
         return "home";
     }
